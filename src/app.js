c5220c2c25c464079f18b8ac30b90c3408131240
lopojo21 <joaco.martino@gmail.com>
2023-09-24 15:07:07 -0300

    se soluciona rutas en app.js

--- src/app.js
+++ src/app.js
@@ -12,12 +12,12 @@ const app=express();
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname,'../views'));
+app.set('views', path.join(__dirname,'./views'));
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.use(express.static(path.join(__dirname,'../public')));
+app.use(express.static(path.join(__dirname,'./public')));
 
 
 app.get('/chat',(req,res)=>{
